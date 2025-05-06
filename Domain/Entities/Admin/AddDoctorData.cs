b871a6124792fb8ee8410482e9b20779873f7e1d
Iongutan <ion.gutan@isa.utm.md>
2025-05-06 21:48:06 +0200

    Implementare servicii AppBL și entități domeniu Admin

---
+++ Domain/Entities/Admin/AddDoctorData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxMed.Domain.Entities.Admin
+{
+     public class AddDoctorData
+     {
+          public string Username { get; set; }
+
+          public string Email { get; set; }
+
+          public string Phone { get; set; }
+
+          public string Type { get; set; }
+
+          public string Description { get; set; }
+
+          public string Image { get; set; }
+     }
+}
