f18b4f1dbf1e44736371b835e7c729eee57f3e62
DanielDEVUTM <daniel.crivoi@isa.utm.md>
2025-05-09 23:25:26 +0300

    Add modify API si ISSesion

---
+++ BusinesLogic/Interfaces/IAdmin.cs
@@ -0,0 +1,22 @@
+﻿using LuxMed.Domain.Entities.Admin;
+using LuxMed.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxMed.BusinessLogic.Interfaces
+{
+    public interface IAdmin
+    {   
+        BoolResp AddUser(AddUserData data);
+        BoolResp AddDoctor(AddDoctorData data);
+        BoolResp EditUser(EditUserData data);
+        BoolResp EditDoctor(EditDoctorData data);
+        BoolResp EditAppointment(EditAppointmentData data);
+        void DeleteUser(int id);
+        void DeleteDoctor(int id);
+        void DeleteAppointment(int id);
+    }
+}
